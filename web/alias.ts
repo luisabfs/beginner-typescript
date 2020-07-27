f6cdb834c39b8a0593587bf09adb695faabb5c52
Luísa Barros <qlullyq@gmail.com>
2020-07-27 01:16:26 -0300

    feat: add union and type alias section

---
+++ web/alias.ts
@@ -0,0 +1,23 @@
+// Type Alias
+type Uid = number | string | undefined;
+type Platform = 'Windows' | 'Linux' | 'MacOS';
+
+function logProduct(uid: Uid, item: string) {
+  console.log(`A product with ${uid} has a title as ${item}.`);
+}
+
+function logUser(uid: Uid, user: string) {
+  console.log(`A product with ${uid} has a title as ${user}.`);
+}
+
+function logPlatform(platform: Platform) {
+  return platform;
+}
+
+logProduct(123, 'cellphone');
+logProduct('123', 'cellphone');
+
+logUser(123, 'Luísa');
+logUser('123', 'Luísa');
+
+logPlatform('Linux');
