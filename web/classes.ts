648c6539f2cdf7bab59b33b3b23a44f24079c4b9
Luísa Barros <qlullyq@gmail.com>
2020-07-27 17:46:31 -0300

    feat: add classes extension

--- web/classes.ts
+++ web/classes.ts
@@ -12,5 +12,20 @@ class UserAccount {
   }
 }
 
+class CharAccount extends UserAccount {
+  nickname: string;
+  level: number;
+
+  constructor(name: string, age: number, nickname: string, level: number) {
+    super(name, age);
+    this.nickname = nickname;
+    this.level = level;
+  }
+}
+
+
 const lu = new UserAccount("luisa", 19);
 lu.logDetails();
+
+const isa = new CharAccount("isalu", 22, "isoca", 149);
+isa.logDetails();
--- web/dist/classes.js
+++ web/dist/classes.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var UserAccount = /** @class */ (function () {
     function UserAccount(name, age) {
         this.name = name;
@@ -9,5 +22,17 @@ var UserAccount = /** @class */ (function () {
     };
     return UserAccount;
 }());
+var CharAccount = /** @class */ (function (_super) {
+    __extends(CharAccount, _super);
+    function CharAccount(name, age, nickname, level) {
+        var _this = _super.call(this, name, age) || this;
+        _this.nickname = nickname;
+        _this.level = level;
+        return _this;
+    }
+    return CharAccount;
+}(UserAccount));
 var lu = new UserAccount("luisa", 19);
 lu.logDetails();
+var isa = new CharAccount("isalu", 22, "isoca", 149);
+isa.logDetails();
