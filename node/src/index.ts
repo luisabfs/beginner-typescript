c3ddb816c7e4c5dc0187df53c36ff837982c2ddf
Luisa Barros <qlullyq@gmail.com>
2020-09-23 16:32:02 -0300

    feat: update index file

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { helloWorld } from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: "Oie" }));
+app.get('/', helloWorld);
 
-app.listen(3333);
+app.listen(3333, () => console.log('✨️ Backend started ✨️'));
